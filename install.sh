eb2b585c99c9dfccd744ec2b32d1637480d9c09f
Aditya Pola <adityapola@gmail.com>
2025-05-09 13:13:01 +0530

    Install bitsandbytes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 conda create -n physic python=3.11
 conda activate physic
 pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu124
-pip install transformers peft wandb pillow pytz
+pip install transformers peft wandb pillow pytz bitsandbytes
