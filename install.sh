9e9a634c90a7eb23fd218649cfd87514e31ca1b8
Aditya Pola <adityapola@gmail.com>
2025-05-09 13:03:11 +0530

    Fix torch-cuda version

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 conda create -n physic python=3.11
 conda activate physic
-conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=11.8 -c pytorch -c nvidia
-pip install transformers peft wandb pillow
+pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu124
+pip install transformers peft wandb pillow pytz
